bf8be1921e6689700c886f5154d9e4c7770b61f9
8devil3 <8dev.il3@gmail.com>
2022-03-04 14:29:44 +0100

    aggiunto timer

--- js/main.js
+++ js/main.js
@@ -3,18 +3,50 @@ Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 30 se
 Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, uno alla volta.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
+const divContainer = document.querySelector(".container");
+const divScore = document.querySelector("#score");
 
-const qtaNum = 5;
-const maxNum = 20;
+const divTimer = document.querySelector("#timer");
+// const btnPlay = document.querySelector("#play");
+// const btnReset = document.querySelector("#reset");
+// const scoreMsg = document.querySelector('#score');
+
+let timeInSeconds = 4; //tempo in secondi
+const qtaNum = 5; //quanti numeri generare
+const maxNum = 20; //numero casuale massimo, il minimo è uno di default
+
+
+
+divContainer.innerHTML = ""; //reset container
+divScore.innerHTML = ""; //reset punteggio
+
+numGenerator();
+timer();
+
+
+function hideNum(){
+    divContainer.innerHTML = "";
+
+    let input;
+    
+    for (let i = 0; i < qtaNum; i++) {
+        input = document.createElement('input');
+        input.type = 'number';
+        input.placeholder = 'Numero da 1 a ' + maxNum;
+        divContainer.appendChild(input);
+    }
+
+}
 
 
 function numGenerator() { //generatore casuale di numeri, arg -> int
     const arrRndmNum = [];
     let rndmNum;
+    let divNumber;
 
     for (let i = 0; i < qtaNum; i++) {
         rndmNum = Math.floor(Math.random() * maxNum) + 1;
-        
+
         while (arrRndmNum.includes(rndmNum)) { //numeri univoci
             rndmNum = Math.floor(Math.random() * maxNum) + 1;
         }
@@ -22,6 +54,24 @@ function numGenerator() { //generatore casuale di numeri, arg -> int
         arrRndmNum.push(rndmNum);
     }
     
-    // console.log(arrRndmNum);
-    return arrRndmNum;
+    for (let x = 0; x < arrRndmNum.length; x++) { //stampo i numeri
+        divNumber = document.createElement('div');
+        divNumber.innerHTML = arrRndmNum[x];
+        divContainer.append(divNumber);
+    }
+
+    
+    
+}
+
+let timeStop = setInterval(timer, 1000); //timer con conto alla rovescia
+
+function timer(){
+    if (timeInSeconds == 0) {
+        hideNum();
+        divTimer.style.display = 'none';
+        clearInterval(timeStop);
+    } else {
+        divTimer.innerHTML = 'Timer: ' + timeInSeconds-- + ' secondi';
+    }
 }
